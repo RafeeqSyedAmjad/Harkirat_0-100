ba61238923d9680d76e270c02d874ac983ebd7d8
Rafeeq <rsyed0904@gmail.com>
2023-12-19 14:47:27 +0530

    Feat: Express Practice

---
+++ ExpressClass/index.js
@@ -0,0 +1,24 @@
+// creating and http server
+// Using Express
+
+const express = require('express');
+
+const app = express(); // Creating a clinic
+
+
+function sum(n){
+    let ans = 0;
+    for (let i = 1; i <= n; i++) {
+        ans = ans + i;   
+    }
+    return ans;
+}
+app.get("/",function(req,res){
+    const n = req.query.n;
+    const ans = sum(n);
+    res.send("Hi your ans is " + ans);
+})
+app.listen(3000);  //Doctor room 
+
+
+//Http server is created
