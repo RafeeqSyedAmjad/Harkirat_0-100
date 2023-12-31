66ba008c74aba27cd7018beed16e53b78d022555
Rafeeq <rsyed0904@gmail.com>
2023-12-31 19:52:40 +0530

    Feat: Started Create ToDo with reac and Nodejs

---
+++ New_Year_App/Backend/index.js
@@ -0,0 +1,18 @@
+// const express = require('express');
+
+// const app = express();
+
+// // Add middleware
+// app.use(express.json());
+
+// // Set up routes
+
+
+// app.post("/todo",function(req,res){
+
+// })
+
+// app.get('/todos', function(req, res) => {
+//     res.send('Hello, wor
+// });
+
---
+++ New_Year_App/README.md
@@ -0,0 +1,9 @@
+The Project contains a simple ToDo application
+
+It has the following feature- 
+
+Anyone can create a todo
+Anyone can create there existing todos
+anyone can mark a todo is done
+
+
